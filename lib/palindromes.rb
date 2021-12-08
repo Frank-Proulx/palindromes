d1430d2b371ba18c056cb74b9b23516b961aa947
Frank-Proulx <frankproulx7@gmail.com>
2021-12-08 15:46:44 -0800

    add method having passed test
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>
    Co-Authored-by: Marisa Edgar <marisaedgar1212@gmail.com>
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

---
+++ lib/palindromes.rb
@@ -0,0 +1,8 @@
+class Palindrome
+
+  def search_palindromes(word)
+    if word  == word.reverse
+      word
+    end
+  end  
+end
--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -1,3 +1,14 @@
 require('rspec')
 require('palindromes')
 
+describe('Palindrome#search_palindromes') do
+  # it('will return the word in reverse') do
+  #   palindrome = Palindrome.new
+  #   expect(palindrome.search_palindromes("pool")).to(eq("loop"))
+  # end
+  
+  it('will check to see if a word is a palindrome and if it is will return the word') do
+    palindrome = Palindrome.new
+    expect(palindrome.search_palindromes("racecar")).to(eq("racecar"))
+  end
+end 
